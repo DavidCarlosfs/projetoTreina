eb8151dac5aaa6652fa97f3056c9014a3d4080a5
DavidCarlosfs <davidcarlosfs@hotmail.com>
2025-02-17 12:29:00 -0300

    Configurações do projeto

---
+++ sprinapi/src/main/java/com/api/sprinapi/SprinapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.sprinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinapiApplication.class, args);
+	}
+
+}
